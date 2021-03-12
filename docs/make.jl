0cfc58f050a06fe365763a8f74faf9383454404e
Rodrigo Chang <rrcp777@gmail.com>
2021-03-11 22:09:35 -0600

    Build everything up

--- docs/make.jl
+++ docs/make.jl
@@ -4,21 +4,15 @@ using Documenter
 makedocs(
     sitename = "Documentación con Julia",
     format = Documenter.HTML(),
-    build   = "page/__site",
     pages = [
         "Inicio" => "index.md", 
         "Demostración" => "another.md"
     ]
-    # modules = [DocumentationSite]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
-# deploydocs(
-#    repo = "github.com/rafaelchp/Documentacion-Julia.git"#, 
-#    #versions = ["stable" => "v^", "v#.#.#"]
-# )
+deploydocs(
+   repo = "github.com/rafaelchp/Documentacion-Julia.git"
+)
